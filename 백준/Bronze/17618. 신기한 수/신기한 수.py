99e461609322dc768566217680486f97fe3bc6f3
chan120714 <94848991+chan120714@users.noreply.github.com>
2023-05-23 00:28:09 +0900

    [Bronze III] Title: 신기한 수, Time: 596 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/17618. 신기한 수/신기한 수.py
+++ 백준/Bronze/17618. 신기한 수/신기한 수.py
@@ -1,8 +1,10 @@
 s=0
 for i in range(1,int(input())+1):
-    a=[]
-    for j in range(len(str(i))):
-        a.append(int(str(i)[j]))
-    if i%sum(a)==0:
+    a=0
+    k=i
+    while k:
+        a+=k%10
+        k//=10
+    if i%a==0:
         s+=1
 print(s)

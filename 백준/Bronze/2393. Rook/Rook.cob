fbb3fd24e5a83dfd492e0c5749f6fe4d9a3d685b
chan120714 <94848991+chan120714@users.noreply.github.com>
2023-06-23 00:21:30 +0900

    [Bronze V] Title: Rook, Time: 12 ms, Memory: 70348 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cob
@@ -0,0 +1,17 @@
+000100 IDENTIFICATION DIVISION.
+000200 PROGRAM-ID. Main.
+000300 PROCEDURE DIVISION.
+000400 DISPLAY "  ___  ___  ___"
+000500 DISPLAY "  | |__| |__| |"
+000600 DISPLAY "  |           |"
+000800 DISPLAY "   \_________/"
+000900 DISPLAY "    \_______/"
+001000 DISPLAY "     |     |"
+001100 DISPLAY "     |     |"
+001200 DISPLAY "     |     |"
+001300 DISPLAY "     |     |"
+001400 DISPLAY "     |_____|"
+001500 DISPLAY "  __/       \__"
+001600 DISPLAY " /             \"
+001700 DISPLAY "/_______________\"
+001800 STOP RUN.

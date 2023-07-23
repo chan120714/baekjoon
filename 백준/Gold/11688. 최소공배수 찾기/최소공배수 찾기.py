1cd521236d6e2f824c30f48cba028606b902aaea
chan120714 <94848991+chan120714@users.noreply.github.com>
2023-07-23 13:34:32 +0900

    [Gold IV] Title: 최소공배수 찾기, Time: 160 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/11688. 최소공배수 찾기/최소공배수 찾기.py
+++ 백준/Gold/11688. 최소공배수 찾기/최소공배수 찾기.py
@@ -3,19 +3,13 @@ n,m,k=map(int,input().split())
 a=[]
 c=[]
 for i in range(1,int(sqrt(k))+1):
-    if k%i==0:
-        a.append(i)
-        c.append(k//i)
+ if k%i==0:a.append(i);c.append(k//i)
 q=n*m//(gcd(n,m))
 for i in a:
-    t=gcd(q,i)
-    if k==q*i//t:
-        print(i)
-        exit()
+ t=gcd(q,i)
+ if k==q*i//t:print(i);exit()
 c.reverse()
 for i in c:
-    t=gcd(q,i)
-    if k==q*i//t:
-        print(i)
-        exit()
+ t=gcd(q,i)
+ if k==q*i//t:print(i);exit()
 print(-1)

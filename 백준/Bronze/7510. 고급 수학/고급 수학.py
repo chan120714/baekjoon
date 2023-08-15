b183ef01fbc3c31dc0ef94ac25179cdea8d86bfc
chan120714 <94848991+chan120714@users.noreply.github.com>
2023-08-16 01:49:46 +0900

    [Bronze III] Title: 고급 수학, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/7510. 고급 수학/고급 수학.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    a=list(map(int,input().split()))
+    a.sort()
+    print(f"Scenario #{i+1}:")
+    if a[0]**2+a[1]**2==a[2]**2:
+        print('yes')
+    else:
+        print('no')
+    print()

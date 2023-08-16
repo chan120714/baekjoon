d2b088d951bcaac9c6315c7871600ec166b32e7f
chan120714 <94848991+chan120714@users.noreply.github.com>
2023-08-17 01:02:06 +0900

    [Diamond V] Title: Power towers, Time: 76 ms, Memory: 43592 KB -BaekjoonHub

---
+++ 백준/Diamond/13970. Power towers/Power towers.py
@@ -0,0 +1,119 @@
+import sys
+input=sys.stdin.readline
+from math import*
+from decimal import*
+from random import*
+p=[2,3,5,7,11,13,17,19,23,29,31,37,41]
+
+def pow(x,y,p):
+    if y<2:
+        return (x**y)%p
+    if y%2:
+        return (pow(x,y//2,p)**2*x)%p
+    else:
+        return (pow(x,y//2,p)**2)%p
+
+def mil(n,a):
+    s,d=0,n-1
+    while not d%2:
+        s+=1
+        d//=2
+    x=pow(a,d,n)
+    if x==1 or x+1==n:
+        return True
+    for i in range(s-1):
+        x=pow(x,2,n)
+        if x+1==n:
+            return True
+    return False
+
+def pri(n):
+    if n in p:
+        return True
+    if n==1 or not n%2:
+        return False
+    for k in p:
+        if not mil(n,k):
+            return False
+    return True
+
+def rh(n,c,d):
+    return (n**2+c)%d
+
+def rho(n):
+    if pri(n):
+        return n
+    if n==1:
+        return 1
+    if not n%2:
+        return 2
+    x,c,d=randint(2,n-1),randint(1,n-1),1
+    y=x
+    while d==1:
+        x=rh(x,c,n)
+        y=rh(y,c,n)
+        y=rh(y,c,n)
+        d=gcd(n,abs(x-y))
+        if d==n:
+            return rho(n)
+    if pri(d):
+        return d
+    return rho(d)
+
+def phi(n):
+    res=Decimal(n)
+    a=[]
+    while n>1:
+        q=rho(n)
+        a.append(q)
+        n//=q
+    a=list(set(a))
+    for i in a:
+        res*=Decimal(1)-(Decimal(1)/Decimal(i))
+    return int(res)
+a,m=map(int,input().split())
+pi=phi(m)
+
+def check(n,l,m):
+    k=1
+    if len(n)-l>4:
+        return 1
+    for i in range(len(n)-1,l-1,-1):
+        if k>20:
+            return 1
+        t=1
+        while k:
+            k-=1
+            if t*n[i]>=m:
+                return 1
+            t*=n[i]
+        k=t
+    return 0
+
+def poww(n,l,m):
+    if n[l]==1:
+        return 1
+    if l==len(n)-1:
+        return n[-1]%m
+    if gcd(n[l],m)==1:
+        return pow(n[l],poww(n,l+1,pi),m)
+    r=poww(n,l+1,pi)
+    if check(n,l+1,pi):
+        return pow(n[l],r+pi,m)
+    return pow(n[l],r,m)
+
+for _ in range(a):
+    if m==1:
+        print(0)
+        continue
+    b,*n=map(int,input().split())
+    if n[0]==1:
+        print(1)
+    l=b
+    for i in range(b):
+        if n[i]==1:
+            l=i
+            break
+    for i in range(b-l):
+        n.pop()
+    print(poww(n,0,m))

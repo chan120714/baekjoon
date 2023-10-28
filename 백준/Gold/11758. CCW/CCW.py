a6237c3455927b1778ee2fed14f6151d9c2f6e3d
chan120714 <94848991+chan120714@users.noreply.github.com>
2023-10-29 02:48:08 +0900

    [Gold V] Title: CCW, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.py
+++ 백준/Gold/11758. CCW/CCW.py
@@ -1,12 +1,9 @@
-import sys
-input=sys.stdin.readline
-pos=[list(map(int,input().split())) for i in range(3)]
-def ccw():
-    p=(pos[1][0]-pos[0][0])*(pos[2][1]-pos[0][1])-((pos[2][0]-pos[0][0])*(pos[1][1]-pos[0][1]))
+def ccw(p1,p2,p3):
+    p=(p2[0]-p1[0])*(p3[1]-p1[1])-(p3[0]-p1[0])*(p2[1]-p1[1])
     if p>0:
-        return 1
+        return 1#시계
     elif p==0:
-        return 0
-    else:
-        return -1
-print(ccw())
+        return 0#직선
+    return -1#반시계
+pos=[list(map(int,input().split())) for i in range(3)]
+print(ccw(pos[0],pos[1],pos[2]))

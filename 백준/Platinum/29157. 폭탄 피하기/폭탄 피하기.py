df82c79de142a7814c94263febfa7fe73d3764d6
chan120714 <chanchan01067@gmail.com>
2025-10-13 03:08:20 +0900

    [Platinum V] Title: 폭탄 피하기, Time: 1008 ms, Memory: 208212 KB -BaekjoonHub

--- 백준/Platinum/29157. 폭탄 피하기/폭탄 피하기.py
+++ 백준/Platinum/29157. 폭탄 피하기/폭탄 피하기.py
@@ -3,6 +3,6 @@ N,M,K=map(int,input().split())
 f=[1]
 for i in range(N+M):f.append(f[-1]*-~i%P)
 b=[list(map(int,input().split()))for i in' '*K]
-C=lambda n,k:f[n]*pow(f[k]*f[n-k]%P,-1,P)
+C=lambda n,k:f[n]*pow(f[k]*f[n-k],-1,P)
 def s(x,y):return C(x+y,x)-sum([C(x-q+y-w,x-q)*s(q,w)for q,w in b if(x>=q and y>=w and(x,y)!=(q,w))])
 print(s(N,M)%P)

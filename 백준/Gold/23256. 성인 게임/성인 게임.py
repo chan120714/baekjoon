4410cbbbd5ba16781e88707727642d08955b1db1
chan120714 <chanchan01067@gmail.com>
2025-10-13 17:26:24 +0900

    [Gold IV] Title: 성인 게임, Time: 700 ms, Memory: 99900 KB -BaekjoonHub

--- 백준/Gold/23256. 성인 게임/성인 게임.py
+++ 백준/Gold/23256. 성인 게임/성인 게임.py
@@ -1,6 +1,3 @@
-M=10**9+7
-a=[7]
-b=3
-for i in range(6**8):a+=(a[i]*3+b*4)%M,;b=(a[i]+2*b)%M
-_,*s=map(int,open(0))
-for l in s:print(a[l-1])
+a=[1,7]
+for i in range(6**8):a+=(a[i+1]*5-2*a[i])%(10**9+7),
+for l in[*open(0)][1:]:print(a[int(l)])

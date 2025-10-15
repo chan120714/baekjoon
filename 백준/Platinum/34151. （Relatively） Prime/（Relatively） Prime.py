0c16bf7d518a6c37a4c1b46851038f3991d56200
chan120714 <chanchan01067@gmail.com>
2025-10-15 13:56:35 +0900

    [Platinum V] Title: (Relatively) Prime, Time: 500 ms, Memory: 112500 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/34151. （Relatively） Prime/（Relatively） Prime.py
+++ 백준/Platinum/34151. （Relatively） Prime/（Relatively） Prime.py
@@ -1,6 +1,10 @@
 import sys
 input=sys.stdin.readline
-MOD=998244353
-for i in range(int(input())):
-    p,n,m=map(int,input().split())
-    print(pow(p,min(n,m),MOD))
+M=998244353
+o=pow
+for _ in range(int(input())):
+    p,n,m=map(int,input().split());
+    if n<m:n,m=m,n
+    x=o(p,m,M);r=n//m*(x==1)or x*(o(x,n//m,M)-1)*o(x-1,-1,M)%M
+    if n%m:r+=o(p,n,M)
+    print(r%M)

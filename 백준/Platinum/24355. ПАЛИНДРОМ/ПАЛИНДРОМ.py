6773226155f6e382f94afefcbb9888e9c50703d8
chan120714 <chanchan01067@gmail.com>
2025-10-18 21:20:37 +0900

    [Platinum IV] Title: ПАЛИНДРОМ, Time: 168 ms, Memory: 141816 KB -BaekjoonHub

---
+++ 백준/Platinum/24355. ПАЛИНДРОМ/ПАЛИНДРОМ.py
@@ -0,0 +1,25 @@
+def manachers(s, n):
+    t='#'+'#'.join(s)+'#'
+    N=2*n+1
+    a=[0]*N
+    r=0
+    p=0
+    for i in range(N):
+        if i<r:
+            a[i]=min(a[2*p-i],r-i)
+        while i-a[i]-1>=0 and i+a[i]+1<N and t[i-a[i]-1]==t[i+a[i]+1]:
+            a[i]+=1
+        if i+a[i]>r:
+            r=i+a[i]
+            p=i
+    return a
+
+
+a=input()
+n=len(a)
+k=manachers(a,n)
+res=0
+for i in range(2*n+1):
+    if i-k[i]<=0:
+        res=max(k[i],res)
+print(n*2-res)

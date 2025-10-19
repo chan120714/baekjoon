530832f5cd2347ef006805f7459a669ca8454c04
chan120714 <chanchan01067@gmail.com>
2025-10-19 11:39:32 +0900

    [Silver II] Title: 폴카의 수학 공부, Time: 464 ms, Memory: 113260 KB -BaekjoonHub

--- 백준/Silver/34077. 폴카의 수학 공부/폴카의 수학 공부.py
+++ 백준/Silver/34077. 폴카의 수학 공부/폴카의 수학 공부.py
@@ -1,5 +1,5 @@
-for _ in' '*int(input()):
- input();a=input()
- s,t=a.find('-'),0 
- for i in range(s+2,len(a)):t=max(t,a[i]>'0')
- print('NO'*(t>0 and s!=-1)or'YES')
+k=input
+for _ in' '*int(k()):
+ k();a=k();s,t=a.find('-'),0 
+ for i in a[s+2:]:t=max(t,i>'0')
+ print('YES'*(t<1 or s<0)or'NO')

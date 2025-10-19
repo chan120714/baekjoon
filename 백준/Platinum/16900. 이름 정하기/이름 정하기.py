6ac0d7f6aa72964595b881d386fa892d632fcc0a
chan120714 <chanchan01067@gmail.com>
2025-10-20 06:44:55 +0900

    [Platinum V] Title: 이름 정하기, Time: 100 ms, Memory: 116452 KB -BaekjoonHub

---
+++ 백준/Platinum/16900. 이름 정하기/이름 정하기.py
@@ -0,0 +1,32 @@
+def fail(s):
+    n=len(s)
+    a=[0]*n
+    st=0
+    for i in range(1,n):
+        while (st and s[i]!=s[st]):
+            st=a[st-1]
+        if s[st]==s[i]:
+            st+=1
+            a[i]=st
+    return a
+
+def kmp(a,b):
+    n=len(a)
+    m=len(b)
+    s=fail(b)
+    res=[]
+    st=0
+    for i in range(n):
+        while (st and a[i]!=b[st]):
+            st=s[st-1]
+        if a[i]==b[st]:
+            if st+1==m:
+                res.append(i-st+1)
+                st=s[st]
+            else:
+                st+=1
+    return res
+
+n,m=map(str,input().split())
+m=int(m)
+print((len(n)-fail(n)[-1])*(m-1)+len(n))

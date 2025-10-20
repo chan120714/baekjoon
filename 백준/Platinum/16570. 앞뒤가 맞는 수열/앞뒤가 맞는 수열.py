89798aae4e550cada8fc3a5be74c5b432132e92e
chan120714 <chanchan01067@gmail.com>
2025-10-20 13:29:44 +0900

    [Platinum V] Title: 앞뒤가 맞는 수열, Time: 312 ms, Memory: 224912 KB -BaekjoonHub

---
+++ 백준/Platinum/16570. 앞뒤가 맞는 수열/앞뒤가 맞는 수열.py
@@ -0,0 +1,37 @@
+def fail(s):
+    n=len(s)
+    a=[0]*n
+    st=0
+    for i in range(1,n):
+        while (st and s[i]!=s[st]):
+            st=a[st-1]
+        if s[st]==s[i]:
+            st+=1
+            a[i]=st
+    return a
+
+def kmp(a,b):
+    n=len(a)
+    m=len(b)
+    s=fail(b)
+    res=[]
+    st=0
+    for i in range(n):
+        while (st and a[i]!=b[st]):
+            st=s[st-1]
+        if a[i]==b[st]:
+            if st+1==m:
+                res.append(i-st+1)
+                st=s[st]
+            else:
+                st+=1
+    return res
+
+n=int(input())
+a=list(map(int,input().split()))
+a.reverse()
+s=fail(a)
+if max(s)==0:
+    print(-1)
+    exit()
+print(max(s),s.count(max(s)))

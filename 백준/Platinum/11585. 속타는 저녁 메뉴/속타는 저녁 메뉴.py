7666693f0c14a8bff9ea44050b0f6b9cc507caca
chan120714 <chanchan01067@gmail.com>
2025-10-20 13:57:48 +0900

    [Platinum V] Title: 속타는 저녁 메뉴, Time: 1576 ms, Memory: 293460 KB -BaekjoonHub

---
+++ 백준/Platinum/11585. 속타는 저녁 메뉴/속타는 저녁 메뉴.py
@@ -0,0 +1,36 @@
+from math import*
+
+def fail(s):
+    n=len(s)
+    a=[0]*n
+    st=0
+    for i in range(1,n):
+        while (st and s[i]!=s[st]):
+            st=a[st-1]
+        if s[st]==s[i]:
+            st+=1
+            a[i]=st
+    return a
+
+def kmp(a,b):
+    n=len(a)
+    m=len(b)
+    s=fail(b)
+    res=[]
+    st=0
+    for i in range(n):
+        while (st and a[i]!=b[st]):
+            st=s[st-1]
+        if a[i]==b[st]:
+            if st+1==m:
+                res.append(i-st+1)
+                st=s[st]
+            else:
+                st+=1
+    return res
+a=int(input())
+n=list(map(str,input().split()))
+m=list(map(str,input().split()))
+n+=n[:-1]
+t=len(kmp(n,m))
+print(f'{t//gcd(t,a)}/{a//gcd(t,a)}')

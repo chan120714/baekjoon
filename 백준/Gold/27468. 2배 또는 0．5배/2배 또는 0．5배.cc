e3c962b6f5cd9059ed1b3f69c8eccbc9caff7b60
chan120714 <chanchan01067@gmail.com>
2025-10-20 16:27:17 +0900

    [Gold V] Title: 2배 또는 0.5배, Time: 224 ms, Memory: 1116 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/27468. 2배 또는 0．5배/2배 또는 0．5배.cc
+++ 백준/Gold/27468. 2배 또는 0．5배/2배 또는 0．5배.cc
@@ -1,20 +1,2 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-	int n;cin >> n; 
-	cout << "YES\n";
-	if (n%2){
-		for (int i=1;i<=n;i++){
-			if (i%2) cout << (i+1)/2+i/4*2 << ' ';
-			else cout << (i+4)/2+(i-1)/4*2 << ' ';
-		}
-	}
-	else{
-		for (int i=2;i<=n;i+=2){
-			if (i%4){
-				cout << i <<' '<<i-1 <<' ';
-			}
-			else cout << i-1<< ' ' <<i <<' '; 
-		}
-	}
-}
+#import<cstdio>
+int x,n;main(){puts("YES");for(scanf("%d",&n);x<n;)printf("%d ","1201"[(++x-n%2)%4]-49+x);}

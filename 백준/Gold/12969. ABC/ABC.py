4e48380c4c0658d9c409182b5a3b533e8b31ff59
chan120714 <chanchan01067@gmail.com>
2025-10-25 14:53:32 +0900

    [Gold I] Title: ABC, Time: 976 ms, Memory: 217316 KB -BaekjoonHub

--- 백준/Gold/12969. ABC/ABC.py
+++ 백준/Gold/12969. ABC/ABC.py
@@ -1,10 +1,9 @@
 n,k=map(int,input().split())
-r=-1
-t={0}
+t={r:=-1}
 def f(s,a,b,c,d):
  global r,t
  if(a,b,c,d)in t or-1!=r or k<d or a+b+c>n:return
- if d==k:r=s+(n-len(s))*'A';return
+ if d==k:r=s+(n-a-b-c)*'A';return
  f(s+'A',a+1,b,c,d);f(s+'B',a,b+1,c,d+a);f(s+'C',a,b,c+1,d+a+b);t|={(a,b,c,d)}
 f('',0,0,0,0)
 print(r)

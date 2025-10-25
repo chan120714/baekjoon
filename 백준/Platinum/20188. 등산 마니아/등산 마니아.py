fd7ee61b260b85699ce95763a0adc77d230c9ac9
chan120714 <chanchan01067@gmail.com>
2025-10-25 23:51:16 +0900

    [Platinum V] Title: 등산 마니아, Time: 424 ms, Memory: 159564 KB, Score: 47 point -BaekjoonHub

---
+++ 백준/Platinum/20188. 등산 마니아/등산 마니아.py
@@ -0,0 +1,22 @@
+import sys
+input=sys.stdin.readline
+
+n=int(input())
+res=-n*(n-1)//2
+g=[[]for i in range(n+1)]
+
+for i in range(n-1):
+    x,y=map(int,input().split())
+    g[x].append(y)
+    g[y].append(x)
+
+def dfs(x,lst):
+    global res
+    t=1
+    for i in g[x]:
+        if i==lst:continue
+        t+=dfs(i,x)
+    res+=t*(t-1)//2+t*(n-t)
+    return t
+dfs(1,-1)
+print(res)

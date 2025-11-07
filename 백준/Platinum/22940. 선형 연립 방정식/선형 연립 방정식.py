effc5bcb068a80f0f3cc5c83475fcc10f01d50d1
chan120714 <chanchan01067@gmail.com>
2025-11-07 22:01:26 +0900

    [Platinum V] Title: 선형 연립 방정식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.py
+++ 백준/Platinum/22940. 선형 연립 방정식/선형 연립 방정식.py
@@ -1,12 +1,19 @@
-from fractions import Fraction
 n=int(input())
-a=[list(map(int,input().split())) for i in range(n)]
+a=[list(map(float,input().split())) for i in range(n)]
 
 for i in range(n):
-    if a[i][i]==0:
-        continue
+    pivot=i
+
     for j in range(i+1,n):
-        t=Fraction(a[j][i],a[i][i])
+        if abs(a[j][i])>abs(a[pivot][i]):
+            pivot=j
+    if a[pivot][i]==0:
+        continue
+
+    a[pivot],a[i]=a[i],a[pivot]
+    
+    for j in range(i+1,n):
+        t=a[j][i]/a[i][i]
         for k in range(i,n+1):
             a[j][k]-=a[i][k]*t
 
@@ -14,8 +21,13 @@ for i in range(n):
 res=[0 for i in range(n)]
 
 for i in range(n-1,-1,-1):
+    if a[i][i]==0:
+        continue
     p=0
-    for j in range(n):
+    for j in range(i+1,n):
        p+=res[j]*a[i][j]
-    res[i]=(a[i][n]-p)//a[i][i]
+    res[i]=(a[i][n]-p)/a[i][i]
+
+for i in range(n):
+    res[i]=round(res[i])
 print(*res)

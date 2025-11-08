e48a05d9622e1bade8d83930a2b54a0c8407a502
chan120714 <chanchan01067@gmail.com>
2025-11-08 21:21:15 +0900

    [Platinum V] Title: 트리와 쿼리, Time: 448 ms, Memory: 34808 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/25402. 트리와 쿼리/트리와 쿼리.cc
+++ 백준/Platinum/25402. 트리와 쿼리/트리와 쿼리.cc
@@ -1,78 +1,80 @@
 #include<bits/stdc++.h>
 using namespace std;
-#pragma GCC optimized("Ofast,un_looped")
-#define MAX 302010
 typedef long long ll;
-typedef pair<ll,ll> pll;
-typedef pair<int,int> pi;
-vector<bool> visited(MAX);
-vector<int> ist(MAX);
-ll comb(ll x){
-	return x*(x-1) >> 1;
+
+ll comb(ll n){
+    return n*(n-1) >> 1;
 }
-vector<int> graph[MAX];
-ll val=0,res=0;
-void dfs(int x){
-	if (visited[x]){
-		return;
-	}
-	visited[x]=true;
-	if (ist[x]==0){
-		return;
-	}
-	else{
-		val++;
-		for (auto i:graph[x]){
-			dfs(i);
-		}
-	}
-	return;
+
+int rpar[342123];
+vector<int> graph[341231];
+
+void dfs(int x,int r){
+    for (auto i:graph[x]){
+        if (i!=r){
+            rpar[i]=x;
+            dfs(i,x);
+        }
+    }
 }
-int k_is_three(int a,int b,int c){
-	int num=0;
-	num+=graph[a][lower_bound(graph[a].begin(),graph[a].end(),c)-graph[a].begin()]==c;
-	num+=graph[a][lower_bound(graph[a].begin(),graph[a].end(),b)-graph[a].begin()]==b;
-	num+=graph[c][lower_bound(graph[c].begin(),graph[c].end(),b)-graph[c].begin()]==b;
-	if (num==0) return 0;
-	if (num==1) return 1;
-	return 3;
+int parent[321321],val[321321];
+int find(int x){
+    if (x==parent[x]) return x;
+    else return parent[x]=find(parent[x]);
 }
+
+void unioned(int x,int y){
+    x=find(x);
+    y=find(y);
+
+    if (x==y) return;
+    if (x>y) swap(x,y);
+    parent[y]=x;
+    val[x]+=val[y];
+    val[y]=0;
+}
+
 int main(){
-	cin.tie(0);cout.tie(0);
-	ios::sync_with_stdio(false);
-	int n;cin >> n;
-	for (int i=1;i<n;i++){
-		int x,y;
-		cin >>x >> y;
-		graph[x].push_back(y);
-		graph[y].push_back(x);
-	}
-	for (int i=1;i<=n;i++){
-		sort(graph[i].begin(),graph[i].end());
-	}
-	int m;cin >> m;
-	while (m--){
-		int a;
-		cin >> a;
-		if (a==3){
-			int x,y,z;cin >>x >> y >>z;
-			cout << k_is_three(x,y,z)<<'\n';
-			continue;
-		}
-		ist.assign(n+1,0);
-		visited.assign(n+1,false);
-		res=0;val=0;
-		for (int i=0;i<a;i++){
-			int p;cin >> p;
-			ist[p]=1;
-		}
-		for (int i=1;i<=n;i++){
-			val=0;
-			if (!visited[i]){
-				dfs(i);
-				res+=comb(val);	
-			}
-		}
-		cout << res<<'\n';
-	}
+    cin.tie(0);cout.tie(0);
+    ios::sync_with_stdio(false);
+    int n;cin >> n;
+
+    for (int i=1;i<n;i++){
+        int x,y;cin >> x >> y;
+        graph[x].push_back(y);
+        graph[y].push_back(x);
+    }
+    dfs(1,-1);
+
+    for (int i=1;i<=n;i++){
+        val[i]=1;
+        parent[i]=i;
+    }
+    int q;cin >> q;
+    while (q--){
+        int k;cin >> k;
+        ll res=0;
+        vector<int> s(k);
+        for (int i=0;i<k;i++) cin >> s[i];
+
+        unordered_set<int> Set;
+
+        for (int i=0;i<k;i++) Set.insert(s[i]);
+
+        for (int i=0;i<k;i++){
+            if (Set.find(rpar[s[i]])!=Set.end()){
+                unioned(s[i],rpar[s[i]]);
+            }
+        }
+
+        for (int i=0;i<k;i++){
+            res+=comb(val[s[i]]);
+        }
+
+        for (int i=0;i<k;i++){
+            val[s[i]]=1;
+            parent[s[i]]=s[i];
+        }
+        cout << res  << '\n';
+    }
 }

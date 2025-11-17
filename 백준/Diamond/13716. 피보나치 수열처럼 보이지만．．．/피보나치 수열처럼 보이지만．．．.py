a9dd27d8fcc4d790f6fad53883a0a34e1fd91647
chan120714 <chanchan01067@gmail.com>
2025-11-17 18:44:32 +0900

    [Diamond V] Title: 피보나치 수열처럼 보이지만..., Time: 132 ms, Memory: 111532 KB -BaekjoonHub

---
+++ 백준/Diamond/13716. 피보나치 수열처럼 보이지만．．．/피보나치 수열처럼 보이지만．．．.py
@@ -0,0 +1,96 @@
+mod=1000000007
+def pow(x,y):
+    res=1
+    while y>0:
+        if y%2:
+            res*=x
+            res%=mod
+        x*=x
+        x%=mod
+        y//=2
+    return res
+def berlekamp_massey(x):
+    ls=[]
+    cur=[]
+    lf,ld=0,0
+    for i in range(len(x)):
+        t=0
+        for j in range(len(cur)):
+            t=(t+x[i-j-1]*cur[j])%mod
+        if (t-x[i])%mod==0:
+            continue
+        if len(cur)==0:
+            cur=[0]*(i+1)
+            lf=i
+            ld=(t-x[i])%mod
+            continue
+        k=-(x[i]-t)*pow(ld,mod-2)%mod
+        c=[0]*(i-lf-1)
+        c.append(k)
+        for j in ls:
+            c.append(-j*k%mod)
+        if len(c)<len(cur):
+            for i in range(len(cur)-len(c)):
+                c.append(0)
+        for j in range(len(cur)):
+            c[j]=(c[j]+cur[j])%mod
+        if (i-lf+len(ls))>=len(cur):
+            ls,lf,ld=cur,i,(t-x[i])%mod
+        cur=c
+    for i in range(len(cur)):
+        cur[i]=(cur[i]%mod+mod)%mod
+    return cur
+def get_nth(rec,dp,n):
+    m=len(rec)
+    s=[0]*m
+    t=[0]*m
+    s[0]=1
+    if m!=1:
+        t[1]=1
+    else:
+        t[0]=rec[0]
+    def mul(v,w):
+        m=len(v)
+        t=[0]*(2*m)
+        for j in range(m):
+            for k in range(m):
+                t[j+k]+=v[j]*w[k]%mod
+                if t[j+k]>=mod:
+                    t[j+k]-=mod
+        for j in range(2*m-1,m-1,-1):
+            for k in range(1,m+1):
+                t[j-k]+=t[j]*rec[k-1]%mod
+                if t[j-k]>=mod:
+                    t[j-k]-=mod
+        if len(t)>m:
+            for i in range(len(t)-m):
+                t.pop()
+        else:
+            for i in range(m-len(t)):
+                t.append(0)
+        return t
+    while n:
+        if n%2:
+            s=mul(s,t)
+        t=mul(t,t)
+        n//=2
+    ret=0
+    for i in range(m):
+        ret+=s[i]*dp[i]%mod
+    return ret%mod
+def guess_nth_term(x,n):
+    if n<len(x):
+        return x[n]
+    v=berlekamp_massey(x)
+    if len(v)==0:
+        return 0
+    return get_nth(v,x,n)
+a=[0]
+f=[1,1,2]
+n,k=map(int,input().split())
+for i in range(3000):
+    f.append(f[-1]+f[-2])
+    f[-1]%=mod
+for i in range(1,3000):
+    a.append((a[-1]+f[i]*pow(i,k))%mod)
+print(guess_nth_term(a,n))

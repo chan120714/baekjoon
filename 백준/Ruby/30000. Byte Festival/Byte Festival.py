ef5b16227455c870266f359e5a558eb3ef9bed7a
chan120714 <chanchan01067@gmail.com>
2025-11-18 09:44:54 +0900

    [Ruby V] Title: Byte Festival, Time: 136 ms, Memory: 111416 KB, Score: 7 point -BaekjoonHub

--- 백준/Ruby/30000. Byte Festival/Byte Festival.py
+++ 백준/Ruby/30000. Byte Festival/Byte Festival.py
@@ -10,3 +10,19 @@ elif x==1:
         else:
             n//=2
     print(1)
+elif x==2:
+    t=['one','ten','one hundred']
+    s='illion'
+    print(*t,sep='\n')
+    for i in t:print(i,'thousand')
+    for a in['','millia','duomillia','tremillia','quattuormillia','quinmillia','sexmillia','septenmillia','octomillia','novemmillia']:
+        for x in['','cen','duocen','trecen','quadringen','quingen','sescen','septingen','octingen','nongen']:
+            if x=='' and a!='':
+                for j in t:print(j,a+x+'t'+s)
+            elif x!='':
+                for j in t:print(j,a+x+s)        
+            for i in['m','b','tr','quadr','quint','sext','sept','oct','non']:
+                for j in t:print(j,a+x+i+s)
+            for i in['dec','vigint','trigint','quadragint','quinquagint','sexagint','septuagint','octogint','nonagint']:
+                for j in['','un','do','tre','quattuor','quin','sex','septen','octo','novem']:
+                    for k in t:print(k,a+x+j+i+s)

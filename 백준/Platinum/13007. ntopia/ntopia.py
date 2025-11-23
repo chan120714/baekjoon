75c59319db42f7c73119627130f07b9a29bdcad4
chan120714 <chanchan01067@gmail.com>
2025-11-24 02:39:33 +0900

    [Platinum V] Title: ntopia, Time: 3288 ms, Memory: 149152 KB -BaekjoonHub

--- 백준/Platinum/13007. ntopia/ntopia.py
+++ 백준/Platinum/13007. ntopia/ntopia.py
@@ -1,9 +1,8 @@
 s,a,b,c=map(int,input().split())
 M=10**9+7
-r=0
 f=[1]
 for i in range(8**7):f+=f[i]*-~i%M,
-for i in range(8**7):f+=[0]
+f+=[r:=0]*8**7
 for t in range(s+1):
     try:r=(r+(-1)**t*f[s]*pow(f[s-t],2,M)*pow(f[a]*f[b]*f[c]*f[t]*f[s-t-a]*f[s-t-b]*f[s-t-c],-1,M))%M
     except:pass

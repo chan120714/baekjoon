f441fd8a444d4b9cea3f4ef2762525d0dbce159d
chan120714 <chanchan01067@gmail.com>
2025-11-24 11:08:39 +0900

    [Platinum V] Title: 과녁 맞히기, Time: 1100 ms, Memory: 486636 KB -BaekjoonHub

--- 백준/Platinum/32390. 과녁 맞히기/과녁 맞히기.py
+++ 백준/Platinum/32390. 과녁 맞히기/과녁 맞히기.py
@@ -3,6 +3,5 @@ M=10**9+7
 f=[1]
 for i in range(7**8):f+=f[i]*-~i%M,
 r=f[n]
-*a,=map(int,input().split())
-for i in a:r=r*pow(f[i],-1,M)*pow(2,i-1,M)%M
+for i in map(int,input().split()):r=r*pow(f[i],-1,M)*pow(2,i-1,M)%M
 print(r)

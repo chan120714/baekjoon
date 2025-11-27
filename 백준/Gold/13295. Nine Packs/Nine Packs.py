1e7031627f0829484eadafc77f4e02fff1d95afe
chan120714 <chanchan01067@gmail.com>
2025-11-27 23:43:18 +0900

    [Gold IV] Title: Nine Packs, Time: 4892 ms, Memory: 33992 KB -BaekjoonHub

---
+++ 백준/Gold/13295. Nine Packs/Nine Packs.py
@@ -0,0 +1,28 @@
+n,*a=map(int,input().split())
+m,*b=map(int,input().split())
+
+d1=[987654321]*101212
+d2=[987654321]*101212
+
+d1[0]=0
+d2[0]=0
+
+for i in a:
+    for j in range(100001,i-1,-1):
+        d1[j]=min(d1[j-i]+1,d1[j])
+
+
+for i in b:
+    for j in range(100001,i-1,-1):
+        d2[j]=min(d2[j-i]+1,d2[j])
+
+
+res=1<<32
+
+for i in range(1,101000):
+    res=min(res,d2[i]+d1[i])
+
+if res>n+m:
+    print('impossible')
+else:
+    print(res)

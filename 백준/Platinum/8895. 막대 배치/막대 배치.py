3260e9caa06757fb13b25d88c77d42ad6668ca2c
chan120714 <chanchan01067@gmail.com>
2025-12-02 18:36:34 +0900

    [Platinum V] Title: 막대 배치, Time: 2736 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Platinum/8895. 막대 배치/막대 배치.py
@@ -0,0 +1,11 @@
+import sys
+input=sys.stdin.readline
+for __ in range(int(input())):
+    n,m,t=map(int,input().split())
+    dp=[[[0 for i in range(n+1)]for i in range(n+1)]for i in range(n+1)]
+    dp[1][1][1]=1
+    for i in range(2,n+1):
+        for j in range(1,i+1):
+            for k in range(1,i+1):
+                dp[i][j][k]=dp[i-1][j-1][k]+dp[i-1][j][k-1]+dp[i-1][j][k]*(i-2)
+    print(dp[n][m][t])

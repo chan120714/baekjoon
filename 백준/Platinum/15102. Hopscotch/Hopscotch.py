093cb5c493ae3e4915c025d29e1943b614dec999
chan120714 <chanchan01067@gmail.com>
2026-02-16 05:51:37 +0900

    [Platinum IV] Title: Hopscotch, Time: 2308 ms, Memory: 72404 KB -BaekjoonHub

--- 백준/Platinum/15102. Hopscotch/Hopscotch.py
+++ 백준/Platinum/15102. Hopscotch/Hopscotch.py
@@ -3,7 +3,4 @@ n,x,y=map(int,input().split())
 f=[1]
 for i in range(n):f+=f[-1]*-~i%M,
 a=0
-for k in range(1,min(n//x,n//y)+1):
- r=k-1;s=n-k*x;t=n-k*y
- a=(a+f[s+r]*f[t+r]*pow(f[r]*f[r]*f[s]*f[t],-1,M))%M
-print(a)
+print(sum(f[n-k*x+k-1]*f[n-k*y+k-1]*pow(f[k-1]**2*f[n-k*x]*f[n-k*y],-1,M) for k in range(1,1+n//max(x,y)))%M)

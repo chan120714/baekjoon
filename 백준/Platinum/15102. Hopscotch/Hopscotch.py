ca51adf680b2bbeef3647f33f1b878093b32ac6b
chan120714 <chanchan01067@gmail.com>
2026-02-16 05:52:53 +0900

    [Platinum IV] Title: Hopscotch, Time: 2192 ms, Memory: 72404 KB -BaekjoonHub

--- 백준/Platinum/15102. Hopscotch/Hopscotch.py
+++ 백준/Platinum/15102. Hopscotch/Hopscotch.py
@@ -2,5 +2,4 @@ M=10**9+7
 n,x,y=map(int,input().split())
 f=[1]
 for i in range(n):f+=f[-1]*-~i%M,
-a=0
 print(sum(f[n-k*x+k-1]*f[n-k*y+k-1]*pow(f[k-1]**2*f[n-k*x]*f[n-k*y],-1,M) for k in range(1,1+n//max(x,y)))%M)

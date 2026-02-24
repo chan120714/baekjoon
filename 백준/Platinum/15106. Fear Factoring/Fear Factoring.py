0568c716d04629f51aa612402e44103ca9dbbc1a
chan120714 <chanchan01067@gmail.com>
2026-02-25 05:22:00 +0900

    [Platinum V] Title: Fear Factoring, Time: 1916 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/15106. Fear Factoring/Fear Factoring.py
+++ 백준/Platinum/15106. Fear Factoring/Fear Factoring.py
@@ -1,11 +1,10 @@
 def f(n):
-    l=1
-    res=0
-    while l<=n:
-        r=n//(n//l)
-        res+=n//l*(l+r)*(r-l+1)//2
-        l=r+1
-    return res
-
+ l=1
+ s=0
+ while l<=n:
+  r=n//(n//l)
+  s+=n//l*(l+r)*(r-l+1)//2
+  l=r+1
+ return s
 a,b=map(int,input().split())
 print(f(b)-f(a-1))

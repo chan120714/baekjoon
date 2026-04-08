c80f1326ec78cbe15767d46cda80bdc535aece02
chan120714 <chanchan01067@gmail.com>
2026-04-09 01:27:04 +0900

    [Platinum V] Title: 바벨탑, Time: 236 ms, Memory: 196948 KB -BaekjoonHub

--- 백준/Platinum/26655. 바벨탑/바벨탑.py
+++ 백준/Platinum/26655. 바벨탑/바벨탑.py
@@ -1,9 +1,7 @@
-n,*a=map(int,open(0).read().split())
 s=[0]
 c=w=p=0
-for x in[*map(lambda x:(x-20)//10,a),0]:
- w+=abs(x-p);p=x
- while s[-1]>x:
-  t=s.pop();c+=(t-max(s[-1],x)+3)//4
- if s[-1]<x:s+=x,
+for x in*map(int,open(0).read().split()[1:]),20:
+ x=x//10-2;w+=abs(x-p);p=x
+ while x<s[-1]:c+=(s.pop()-max(s[-1],x)+3)//4
+ if x-s[-1]:s+=x,
 print(w*10,c*4)
